a0a7c67a844f5f725bd990b1ee0c64e75ff18e3a
“Gabriel <angelhumano47@gmail.com>
2023-09-12 17:48:14 -0400

    working on insights

---
+++ src/main.py
@@ -0,0 +1,15 @@
+
+
+# Allows us to connect to the data source and pulls the information
+from sodapy import Socrata
+import requests
+from requests.auth import HTTPBasicAuth
+import json 
+import argparse
+import sys
+import os
+
+# The script is not included here since the professor may use a similar project for her class.
+#If you are not a student, I can show you the code upon request.
+
+
